8415ccf5e7c9fb6572191228a6a0711107c6918d
Hariom Gautam <138903615+OmGautam123@users.noreply.github.com>
2024-10-24 17:06:32 +0530

    Add files via upload

---
+++ listing.js
@@ -0,0 +1,27 @@
+const mongoose  = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type: Object,
+        default: {
+            filename: "listingimage",
+            url: "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG90ZWxzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+          },
+        set: (v)=> v==="" ? {
+            filename: "listingimage",
+            url: "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG90ZWxzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+          }: v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+}); 
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
